d76b45d62f7601025e8b1071e3f011f6d7b6b5ee
Hernan Velasquez <velasquezmhernan@gmail.com>
2022-06-19 19:31:24 -0500

    Agregando el metodo de obtener la informacion

--- js/main.axios.js
+++ js/main.axios.js
@@ -1,19 +1,34 @@
-const dom = document,
-    $table = dom.querySelector('.crud-table'),
-    $form = dom.querySelector('.crud-form'),
-    $title = dom.querySelector('.crud-title'),
-    $templeate = dom.getElementById('crud-template'),
-    $fragment = dom.createDocumentFragment();
+const d = document,
+    $table = d.querySelector(".crud-table"),
+    $form = d.querySelector(".crud-form"),
+    $title = d.querySelector(".crud-title"),
+    $template = d.getElementById("crud-template").content,
+    $fragment = d.createDocumentFragment();
 
-const getAll = async () =>{
-    try {
-        let res = await axios.get("http://localhost:5555/santos");
-        let json = await res.data;
-        console.log(json);
-    } catch (err) {
-        let message = err.statusText || 'Ocurrió un error';
-        $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+    /**
+     * Metodo encargado de realizar el llamado a la API 
+     * para obtener la informacion de los datos.
+     */
+    const getAll = async () => {
+        try {
+            let res =await axios.get("http://localhost:5555/santos"),
+            json = await res.data;
+            console.log(json);
+            json.forEach(el => {
+                $template.querySelector(".name").textContent = el.nombre;
+                $template.querySelector(".constellation").textContent = el.constelacion;
+                $template.querySelector(".edit").dataset.id = el.id;
+                $template.querySelector(".edit").dataset.name = el.nombre;
+                $template.querySelector(".edit").dataset.constellation = el.constelacion;
+                $template.querySelector(".delete").dataset.id = el.id;
+    
+                let $clone = d.importNode($template, true);
+                $fragment.appendChild($clone);
+            });
+            $table.querySelector("tbody").appendChild($fragment);
+        } catch (error) {
+            let message = err.statusText || "Ocurrio un error";
+            $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}:${message}</b></p>`);
+        }
     }
-}
-
-dom.addEventListener("DOMContentLoaded", getAll)
+    d.addEventListener("DOMContentLoaded", getAll)
